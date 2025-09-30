473dd193318f06432ed5a927954e26b758b9ac3b
aritzramos <aritz.ramos.lozares.al@iespoligonosur.org>
2025-09-30 11:15:03 +0200

    30/09 me he quedado en el admin

---
+++ proyecto1/animales/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+
+class Animales(models.Model):
+    cuidador = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=60)
+    tipo = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return "Cuidador: " + self.cuidador + ", Nombre: " + self.nombre + "Tipo: " + self.tipo
+
+class Protectora(models.Model):
+    nombre = models.CharField(max_length=60)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return "Nombre: " + self.nombre + ", Descripción: " + self.descripcion + "Fecha: " + self.fecha_creacion
+    
+class Colaborador(models.Model):
+    nombre = models.CharField(max_length=60)
+    cargo = models.CharField(max_length=20)
+    fecha_entrada_protectora = models.DateTimeField()
+    
+    def __str__(self):
+        return "Nombre: " + self.nombre + ", Cargo: " + self.cargo + "Fecha de entrada: " + self.fecha_entrada_protectora
