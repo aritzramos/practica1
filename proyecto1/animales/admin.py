e0334db8dda4cab16124757f550e59fd933e82b3
aritzramos <aritz.ramos.lozares.al@iespoligonosur.org>
2025-10-02 12:17:37 +0200

    proyecto1 terminado

--- proyecto1/animales/admin.py
+++ proyecto1/animales/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Animales, Protectora, Colaborador
 
 # Register your models here.
+
+admin.site.register(Animales),
+admin.site.register(Protectora),
+admin.site.register(Colaborador)
--- proyecto1/animales/views.py
+++ proyecto1/animales/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Animales, Protectora, Colaborador
 
 # Create your views here.
+
+def main(request):
+    return render(request, 'animales/main.html', {})
+
+def animales(request):
+    animales = Animales.objects.all()
+    return render(request, 'animales/animales.html', {'animales_mostrar': animales})
+
+def protectora(request):
+    protectora = Protectora.objects.all()
+    return render(request, 'animales/protectora.html', {'protectora_mostrar': protectora})
+
+def colaborador(request):
+    colaborador = Colaborador.objects.all()
+    return render(request, 'animales/colaborador.html', {'colaborador_mostrar': colaborador})
